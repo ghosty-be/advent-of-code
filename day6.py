d7384e2ee548f71e7bf42ac8b67c1d08dd371642
ghosty-be <ghosty.be@gmail.com>
2020-12-07 03:03:48 +0100

    day 6 cleaned

--- day6.py
+++ day6.py
@@ -2,6 +2,33 @@
 
 """
 6a:
+form asks a series of 26 yes-or-no questions marked a through z
+Each group's answers are separated by a blank line, and within each group, each person's answers are on a single line.
+see day6-example.txt
+
+This list represents answers from five groups:
+
+The first group contains one person who answered "yes" to 3 questions: a, b, and c.
+The second group contains three people; combined, they answered "yes" to 3 questions: a, b, and c.
+The third group contains two people; combined, they answered "yes" to 3 questions: a, b, and c.
+The fourth group contains four people; combined, they answered "yes" to only 1 question, a.
+The last group contains one person who answered "yes" to only 1 question, b.
+In this example, the sum of these counts is 3 + 3 + 3 + 1 + 1 = 11.
+
+For each group, count the number of questions to which anyone answered "yes". What is the sum of those counts?
+
+6b:
+You don't need to identify the questions to which anyone answered "yes"; you need to identify the questions to which everyone answered "yes"!
+for the example:
+
+In the first group, everyone (all 1 person) answered "yes" to 3 questions: a, b, and c.
+In the second group, there is no question to which everyone answered "yes".
+In the third group, everyone answered yes to only 1 question, a. Since some people did not answer "yes" to b or c, they don't count.
+In the fourth group, everyone answered yes to only 1 question, a.
+In the fifth group, everyone (all 1 person) answered "yes" to 1 question, b.
+In this example, the sum of these counts is 3 + 0 + 1 + 1 + 1 = 6.
+
+For each group, count the number of questions to which everyone answered "yes". What is the sum of those counts?
 """
 
 import sys
@@ -31,12 +58,14 @@ file.close()
 # sum(alphabet.values())
 # after that reset the alphabet dict back to 0
 # alphabet = dict.fromkeys(alphabet, 0)
+# 6b
+# instead of setting the chars to 1 count the occurence and count all chars with an occurence = amount of ansers in the group
+# so just abandon the initial = 1 and sum but replace by += 1 and check for > 0 for 6a
 
 def processanswers(answerform):
     totalsum = 0
     totalsum2 = 0
     alphabet = dict.fromkeys(string.ascii_lowercase, 0)
-    alphabet2 = dict.fromkeys(string.ascii_lowercase, 0)
     for groupanswers in answerform:
         groupmembers = 0
         groupsum = 0
@@ -45,9 +74,9 @@ def processanswers(answerform):
         for answer in answers:
             groupmembers += 1
             for char in answer:
-                #alphabet[char] = 1
+                #6a: alphabet[char] = 1
                 alphabet[char] += 1
-        #groupsum = sum(alphabet.values())
+        #6a: groupsum = sum(alphabet.values())
         for value in alphabet.values():
             if value > 0:
                 groupsum += 1
